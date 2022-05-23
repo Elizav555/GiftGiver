74a79cf57ff88a367a8f15af13aa64dc1529720c
Elizav555 <lizagarkina5@gmail.com>
2022-05-23 10:04:16 +0300

    changes in room

--- app/src/main/java/com/example/giftgiver/common/db/room/RoomDB.kt
+++ app/src/main/java/com/example/giftgiver/common/db/room/RoomDB.kt
@@ -1,8 +1,8 @@
 package com.example.giftgiver.common.db.room
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import androidx.room.*
+import androidx.room.migration.AutoMigrationSpec
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.giftgiver.common.db.room.converters.DateConverter
 import com.example.giftgiver.common.db.room.converters.ListsConverter
 import com.example.giftgiver.features.client.data.room.ClientDao
@@ -22,13 +22,39 @@ import com.example.giftgiver.features.wishlist.data.room.WishlistR
         EventR::class,
         WishlistR::class
     ],
-    version = 4
+    version = 5,
+    exportSchema = true,
+    autoMigrations = [
+        AutoMigration(from = 3, to = 4, spec = RoomDB.MyExampleAutoMigration::class),
+        AutoMigration(from = 4, to = 5)
+    ]
 )
+
+
 abstract class RoomDB : RoomDatabase() {
     abstract fun clientDao(): ClientDao
     abstract fun giftDao(): GiftDao
 
     companion object {
         const val DATABASE_NAME = "gift_giver.db"
+    }
+
+    @DeleteColumn(
+        tableName = "clients",
+        columnName = "giftsInfo"
+    )
+    @DeleteColumn(
+        tableName = "clients",
+        columnName = "wishlists"
+    )
+    @DeleteColumn(
+        tableName = "clients",
+        columnName = "events"
+    )
+    class MyExampleAutoMigration : AutoMigrationSpec {
+        @Override
+        override fun onPostMigrate(db: SupportSQLiteDatabase) {
+            // Invoked once auto migration is done
+        }
     }
 }
